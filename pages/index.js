4595cb10eb8a24e5440a24b98af100f438da479f
Sarvesh Shelgaonkar <115150854+Sarvesh-Shelgaonkar@users.noreply.github.com>
2025-07-24 17:45:21 +0530

    Website_FaterLoading_Problem_Solved

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
 import Ubuntu from "../components/ubuntu";
 import ReactGA from 'react-ga4';
 import Meta from "../components/SEO/Meta";
+import axios from "axios";
 
 const TRACKING_ID = process.env.NEXT_PUBLIC_TRACKING_ID;
 if (TRACKING_ID) {
   ReactGA.initialize(TRACKING_ID);
 }
+
+const url = `https://portfolio-ubuntu.onrender.com/`;
+const interval = 30000;
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log("website reloded");
+    })
+    .catch((error) => {
+      console.error(`Error : ${error.message}`);
+    });
+}
+
+setInterval(reloadWebsite, interval);
 
 function App() {
   return (
